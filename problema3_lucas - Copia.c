8c1afc34607b877c3ea1fd8d16ba72734b75d88b
lucas2213690 <lucas2213690@gmail.com>
2016-03-28 23:52:38 +0200

    Update problema3_lucas - Copia.c

--- problema3_lucas - Copia.c
+++ problema3_lucas - Copia.c
@@ -8,56 +8,56 @@ apostilas, e páginas ou documentos eletrônicos da Internet, qualquer trecho de
 de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
 do código, e estou ciente que estes trechos não seráo considerados para fins de avaliação
 *****************************************************************************************/
-
 #include<stdio.h>
 #include<stdbool.h>
 #include<stdlib.h>
-#include<strings.h>
+#include<string.h>
 
 #define CIDADE 10
 #define DIA 30
 #define HORA 24
 
-typedef struct_dados{	// Estrutura de Dados de Entrada
-	// a hora nao é armazenada, já que tem um indice para isso
+struct dados{	// Estrutura de Dados de Entrada
+	// a hora nao e armazenada, ja que tem um indice para isso
     float temp;
     float pressao;
     float umidade;
     float prec;
     float km;
     char vento[3];
-} dados;
-typedef struct_saida{		//Estrutura de Dados de Saida
+};
+struct saida{		//Estrutura de Dados de Saida
 	float temp;
 	int cidade;
-}saida;
+};
 
-void selection_sort(saida []);
-void leitura(dados [][DIA][HORA], int,char [],char []);
-void temp_minima(dados [][DIA][HORA], int);
-void temp_media(dados [][DIA][HORA], int);
-void temp_max(dados [][DIA][HORA], int);
-void medias(dados [][DIA][HORA],int);
-void consulta(char[],int,int );
-void desconversor(char [],int);
+void selection_sort(struct saida []);
+void leitura(struct dados [][DIA][HORA], int, char [],char []);
+void temp_minima(struct dados [][DIA][HORA], int);
+void temp_media(struct dados [][DIA][HORA], int);
+void temp_max(struct dados [][DIA][HORA], int);
+void medias(struct dados [][DIA][HORA], int);
+void consulta(struct dados [][DIA][HORA], char[], int, int );
+void desconversor(char [], int);
 int conversor(char []);
 
 int main (void)
 {
 	int escolha = 0;
 	char sigla[3];
-	char dia[3];
+	char string_dia[3];
 	char mes[3];
 	int count=0;
-	int dia_media;
-	dados matriz[CIDADE][DIA][HORA];
+	int dia;
+	int hora;
+	struct dados matriz[CIDADE][DIA][HORA];
 	while(true)
 	{
 		printf("######################	\n");
 		printf(" 1 - Importar dados	\n");
 		printf(" 2 - Exportar dados	\n");
 		printf(" 3 - Consultar		\n");
-		printf(" 4 - Médias Diárias	\n");
+		printf(" 4 - Medias Diarias	\n");
 		printf(" 5 - Sair		\n");
 		printf("######################	\n");
 		scanf("%d",&escolha);
@@ -65,46 +65,48 @@ int main (void)
 		{
 			case 1:
 				printf("Digite de qual dia deseja realizar a leitura, Exemplo: 02,05...\n");
-				scanf(" %s",dia);
+				scanf(" %s",string_dia);
 				printf("Digite qual mes  Exemplo:08,09....\n");
 				scanf(" %s",mes);
 
 				if (count <= 29)
-					leitura(dados matriz[][DIA][HORA],count,dia,mes);
+					leitura(matriz, count, string_dia, mes);
 				else
 				{
 					count = 0;
-					leitura(dados matriz[][DIA][HORA],count,dia,mes);
+					leitura(matriz, count, string_dia, mes);
 				}
 				count++;
 
 				break;
 			case 2:
-				temp_minima(dados matriz[][DIA][HORA]);
-				temp_max(dados matriz[][DIA][HORA]);
-				temp_media(dados matriz[][DIA][HORA]);
+				printf("Digite de qual dia deseja exportar os dados");
+				scanf("%d",&dia);
+				temp_minima(matriz, dia);
+				temp_max(matriz, dia);
+				temp_media(matriz, dia);
 				printf("Arquivos gerados com sucesso\n");
 				break;
 			case 3:
-				printf("Informe a sigla, o dia e a hora que deseja consultar, separado por um espaço\n");
+				printf("Informe a sigla, o dia e a hora que deseja consultar, separado por um espaco\n");
 				printf("Exemplo: SSA 13 22");
-				__fpurge(stdin)
+				__fpurge(stdin);
 				scanf("%s %d %d",sigla ,&dia ,&hora);
-				consulta(sigla,dia,hora);
+				consulta(matriz, sigla, dia, hora);
 				break;
 			case 4:
-				printf("Digite o dia em que deseja gerar as médias");
-				scanf("%d",&dia_media);
-				medias(dados matriz[][DIA][HORA], dia_media);
+				printf("Digite o dia em que deseja gerar as medias");
+				scanf("%d",&dia);
+				medias(matriz, dia);
 			case 5:
 				return 0;
 			default:
-				printf("Comando Inválido \n");
+				printf("Comando Invalido \n");
 				break;
 		}
 	}
 }
-void leitura(dados matriz[][DIA][HORA], int count,char dia[],char mes[])  // testado
+void leitura(struct dados matriz[][DIA][HORA], int count,char dia[],char mes[])  // testado
 {
 	char nome_arquivo[20];
 	int hora = 0;
@@ -121,10 +123,10 @@ void leitura(dados matriz[][DIA][HORA], int count,char dia[],char mes[])  // tes
 
 		file = fopen(nome_arquivo, "r");
 		if (file == NULL){
-			printf("Arquivo não encontrado");
+			printf("Arquivo nao encontrado");
 			exit(1);
 		}
-		while( (fscanf(file,"%d %f %f %f %f %f %s\n", &aux, &matriz[cidade][count][hora].temp, &matriz[cidade][count][hora].pressao, &matriz[cidade][count][hora].umidade,&matriz[cidade][count][hora].prec, &matriz[cidade][count][hora].km, matriz[cidade][count][hora].vento))!=EOF ){
+		while( (fscanf(file,"%d %f %f %f %f %f %s\n", &aux, &matriz[i][count][hora].temp, &matriz[i][count][hora].pressao, &matriz[i][count][hora].umidade,&matriz[i][count][hora].prec, &matriz[i][count][hora].km, matriz[i][count][hora].vento))!=EOF ){
 		hora++;
 	}
 	fclose(file);
@@ -132,18 +134,19 @@ void leitura(dados matriz[][DIA][HORA], int count,char dia[],char mes[])  // tes
 	}
 
 }
-void consulta(char sigla[3], int dia, int hora)
+void consulta(struct dados matriz[][DIA][HORA], char sigla[3], int dia, int hora)
 {
 	int cidade = conversor(sigla);
-	printf("Temperatura: |%f|,  Pressão Atmosferica: |%f|,  Umidade: |%f|,  Precipitação: |%f|, Km: |%f|,  Vento:  |%s|",matriz[cidade][dia][hora].temp,matriz[cidade][dia][hora].pressao,matriz[cidade][dia][hora].umidade,matriz[cidade][dia][hora].prec,matriz[cidade][dia][hora].km,matriz[cidade][dia][hora].vento);
+	printf("Temperatura: |%f|,  Pressao Atmosferica: |%f|,  Umidade: |%f|,  Precipitacao: |%f|, Km: |%f|,  Vento:  |%s|",matriz[cidade][dia][hora].temp,matriz[cidade][dia][hora].pressao,matriz[cidade][dia][hora].umidade,matriz[cidade][dia][hora].prec,matriz[cidade][dia][hora].km,matriz[cidade][dia][hora].vento);
 
 }
-void temp_minima(dados matriz[][DIA][HORA], int dia_escolhido)
+void temp_minima(struct dados matriz[][DIA][HORA], int dia_escolhido)
 {
+	FILE *file;
 	int minima;
 	char nome[3];
 	char temperatura[10];
-	saida vet[10];
+	struct saida vet[10];
 	int c;
 	int aux = 0;
 	int h;
@@ -157,12 +160,12 @@ void temp_minima(dados matriz[][DIA][HORA], int dia_escolhido)
 		minima = matriz[c][dia_escolhido][0].temp;
 		for(h = 0; h < 24; h++)
 		{
-			if (matriz[c][dia_escolhido][h] < minima)
-				minima = matriz[c][dia_escolhido][h];
+			if (matriz[c][dia_escolhido][h].temp < minima)
+				minima = matriz[c][dia_escolhido][h].temp;
 		}
 		vet[c].temp = minima;
 	}
-	selection_sort(saida vet);
+	selection_sort(vet);
 
 
 	file = fopen("temp_minima.txt", "w"); // cria um arquivo
@@ -173,11 +176,12 @@ void temp_minima(dados matriz[][DIA][HORA], int dia_escolhido)
 	}
 	fclose(file);
 }
-void temp_media(dados matriz[][DIA][HORA], int dia_escolhido)
+void temp_media(struct dados matriz[][DIA][HORA], int dia_escolhido)
 {
+	FILE *file;
 	char nome[3];
 	char temperatura[10];
-	saida vet[10];
+	struct saida vet[10];
 	int c;
 	int aux = 0;
 	int h;
@@ -189,7 +193,7 @@ void temp_media(dados matriz[][DIA][HORA], int dia_escolhido)
 
 	for(c = 0;c < 10; c++)
 		for(h = 0;h < 24; h++)
-			vet[c].temp += matriz[c][dia_escolhido][h];
+			vet[c].temp += matriz[c][dia_escolhido][h].temp;
 
 
 	selection_sort(vet);
@@ -203,12 +207,13 @@ void temp_media(dados matriz[][DIA][HORA], int dia_escolhido)
 	}
 	fclose(file);
 }
-void temp_max(dados matriz[][DIA][HORA], int dia_escolhido)
+void temp_max(struct dados matriz[][DIA][HORA], int dia_escolhido)
 {
+	FILE *file;
 	int maxima;
 	char nome[3];
 	char temperatura[10];
-	saida vet[10];
+	struct saida vet[10];
 	int c;
 	int aux = 0;
 	int h;
@@ -222,8 +227,8 @@ void temp_max(dados matriz[][DIA][HORA], int dia_escolhido)
 		maxima = matriz[c][dia_escolhido][0].temp;
 		for(h = 0;h < 24; h++)
 		{
-			if (matriz[c][dia_escolhido][h] > maxima)
-				maxima = matriz[c][dia_escolhido][h];
+			if (matriz[c][dia_escolhido][h].temp > maxima)
+				maxima = matriz[c][dia_escolhido][h].temp;
 		}
 		vet[c].temp = maxima;
 	}
@@ -240,7 +245,7 @@ void temp_max(dados matriz[][DIA][HORA], int dia_escolhido)
 
 
 }
-void medias(dados [][DIA][HORA],int dia);
+void medias(struct dados matriz[][DIA][HORA], int dia)
 {
 	FILE *file;
 	char sigla[3];
@@ -248,7 +253,7 @@ void medias(dados [][DIA][HORA],int dia);
 	int h;
 	float media_temp = 0;
 	float media_pressao = 0;
-	float media_umidade = 0
+	float media_umidade = 0;
 	float media_prec = 0;
 	file = fopen("medias_diarias.txt", "w");
 	for(i = 0; i < 10; i++)
@@ -261,13 +266,13 @@ void medias(dados [][DIA][HORA],int dia);
 	      media_umidade += matriz[i][dia][h].umidade;
 	      media_prec += matriz[i][dia][h].prec;
 	    }
-	  fprintf(file,"CIDADE: %s  Temperatura: %.2f  Pressao: %.2f  Umidade: %.2f  Precipitacao: %.2f \n", sigla, media_temp/24, media_pressao/24, media_umidade/24, media_prec/24);
+	    fprintf(file,"CIDADE: %s  Temperatura: %.2f  Pressao: %.2f  Umidade: %.2f  Precipitacao: %.2f \n", sigla, media_temp/24, media_pressao/24, media_umidade/24, media_prec/24);
 	  
 	}
 	fclose(file);
 
 }
-int conversor(char sigla[3])
+int conversor(char sigla[])
 {
 		if (!strcmp(sigla,"SSA"))
 			return 0;
@@ -279,18 +284,16 @@ int conversor(char sigla[3])
 			return 3;
 		else if (!strcmp(sigla, "ITA"))
 			return 4;
-		else if (!strcmp(sigla, "  "))
-			return 5
-		else if (!strcmp(sigla, "  "))
-			return 6
-		else if (!strcmp(sigla, "  "))
-			return 7
-		else if (!strcmp(sigla, "  "))
-			return 8
-		else if (!strcmp(sigla, "  "))
-			return 9
-	}
-	return (cidade);
+		else if (!strcmp(sigla, "JUA"))
+			return 5;
+		else if (!strcmp(sigla, "LDF"))
+			return 6;
+		else if (!strcmp(sigla, "ILH"))
+			return 7;
+		else if (!strcmp(sigla, "JEQ"))
+			return 8;
+		else if (!strcmp(sigla, "TDF"))
+			return 9;
 }
 void desconversor(char nome[],int sigla)
 {
@@ -301,7 +304,7 @@ void desconversor(char nome[],int sigla)
 			break;
 		case 1:
 			strcpy(nome, "FSA_");
-			break:
+			break;
 		case 2:
 			strcpy(nome, "VDC_");
 			break;
@@ -312,23 +315,23 @@ void desconversor(char nome[],int sigla)
 			strcpy(nome, "ITA_");
 			break;
 		case 5:
-			strcpy(nome, "_");
+			strcpy(nome, "JUA_");
 			break;
 		case 6:
-			strcpy(nome, "_");
+			strcpy(nome, "LDF_");
 			break;
 		case 7:
-			strcpy(nome, "_");
+			strcpy(nome, "ILH_");
 			break;
 		case 8:
-			strcpy(nome, "_");
+			strcpy(nome, "JEQ_");
 			break;
 		case 9:
-			strcpy(nome, "_");
+			strcpy(nome, "TDF_");
 			break;
 	}
 }
-void selection_sort(saida vet[])
+void selection_sort(struct saida a[])
 {
 
     int i, j, min,aux;
@@ -338,14 +341,14 @@ void selection_sort(saida vet[])
         min = i;
         for (j = i + 1; j < 10; j++)
         {
-            if (a[j] < a[min])
+            if (a[j].temp < a[min].temp)
                 min = j;
         }
 		//troca a temperatura
 		aux = a[i].temp;
 		a[i].temp = a[min].temp;
 		a[min].temp = aux;
-		//o campo CIDADE também acompanha a troca para nao se perder
+		//o campo CIDADE tambem acompanha a troca para nao se perder
 		aux = a[i].cidade;
 		a[i].cidade = a[min].cidade;
 		a[min].cidade = aux;
